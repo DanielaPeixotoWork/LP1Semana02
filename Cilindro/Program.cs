06a05d8d98f30faec164709a3e43e9ea439bcbfc
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-02-28 19:55:05 +0000

    Altura do Cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        Console.WriteLine("Cálculo do volume e área de superfície de um cilindro");
+
+        Console.Write("Insira a altura (a) do cilindro: ");
+        float altura = float.Parse(Console.ReadLine());
+
+        }
+    }
+}
