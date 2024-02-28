7e01b86f3870546cadff3c43317884f6a2b29112
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-02-28 21:23:28 +0000

    Valores minimos e maximos dos valores especiais

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Todos os Mínimos e Máximos de todos os valores especiais:");
+
+            Console.WriteLine($"Valor Mínimo de sbyte: {sbyte.MinValue}");
+            Console.WriteLine($"Valor Máximo de sbyte: {sbyte.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de byte: {byte.MinValue}");
+            Console.WriteLine($"Valor Máximo de byte: {byte.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de short: {short.MinValue}");
+            Console.WriteLine($"Valor Máximo de short: {short.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de ushort: {ushort.MinValue}");
+            Console.WriteLine($"Valor Máximo de ushort: {ushort.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de int: {int.MinValue}");
+            Console.WriteLine($"Valor Máximo de int: {int.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de uint: {uint.MinValue}");
+            Console.WriteLine($"Valor Máximo de uint: {uint.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de long: {long.MinValue}");
+            Console.WriteLine($"Valor Máximo de long: {long.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de ulong: {ulong.MinValue}");
+            Console.WriteLine($"Valor Máximo de ulong: {ulong.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de float: {float.MinValue}");
+            Console.WriteLine($"Valor Máximo de float: {float.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de double: {double.MinValue}");
+            Console.WriteLine($"Valor Máximo de double: {double.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de decimal: {decimal.MinValue}");
+            Console.WriteLine($"Valor Máximo de decimal: {decimal.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de char: {(int)char.MinValue}");
+            Console.WriteLine($"Valor Máximo de char: {(int)char.MaxValue}");
+
+            Console.WriteLine($"Valor Mínimo de bool: {bool.FalseString}");
+            Console.WriteLine($"Valor Máximo de bool: {bool.TrueString}");
+        }
+    }
+}
