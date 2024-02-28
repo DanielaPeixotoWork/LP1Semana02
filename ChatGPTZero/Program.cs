6b6683a54a2040da8beb209b56b21b1c27ea3194
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-02-28 21:48:12 +0000

    ChatGPTZero finished

---
+++ ChatGPTZero/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChatGPTZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string question, response;
+
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            switch (question)
+
+            {
+                case "Are you more intelligent than me?":
+                    response = "Obviously!";
+                    break;
+
+            }
+
+             Console.WriteLine(response);
+        }
+    }
+}
