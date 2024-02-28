42cd6195b81c12ded89704ac103310ae50a24ae7
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-02-28 22:29:18 +0000

    ChatGPTOne finalizado

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ask, ans;
+
+            while (true)
+            { 
+
+                Console.Write("Pode fazer-me perguntas? ");
+                ask = Console.ReadLine();
+
+                if (ask == "EXIT")
+                {
+                    break;
+                }
+
+                switch (ask)
+                {
+                    case "Qual é o tempo para amanha?":
+                        ans = "Nublado com chuviscos.";
+                        break;
+                    case "Qual é o teu nome?":
+                        ans = "ChatGPTOne.";
+                        break;
+                    case "O que achas de computação?":
+                        ans = "Perfeita!";
+                        break;
+                    case "Gostas de música?":
+                        ans = "Sim, especialmente Pop!";
+                        break;
+                    default:
+                        ans = "Desculpe-me, não consigo responder a isso...";
+                        break;
+                }
+
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
