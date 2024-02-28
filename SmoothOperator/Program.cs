e709b396d8fc5c6965fd3498cf9a0571d6457670
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-02-28 15:21:34 +0000

    First commit

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira um número não-negativo:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input < 0)
+            {
+                Console.WriteLine("Erro,Insira um numero inteiro não-negativo");
+                return;
+            }
+
+            byte n = Convert.ToByte(input);
+
+            Console.WriteLine($"Resultado de -n:{--n}");
+            Console.WriteLine($"Resultado de +n:{++n}");
+        }
+    }
+}
